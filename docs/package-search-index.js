c01078ff0a5bf1b37b9b6ec1a75e6c48e16074b3
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-07-05 23:27:20 -0600

    Updated files in the `docs` directory

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"utilities"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"tests"},{"l":"utilities"}];updateSearchResults();
